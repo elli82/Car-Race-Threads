9a7a580e9693f45260d26c97ab51063bd6a30eb6
elli82 <114058163+elli82@users.noreply.github.com>
2023-04-28 18:55:49 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Car_Race_Threads //Elin Linderholm SUT22
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int distancetoRace =0;
+
+        public Car(string _name, int _speed)
+        {
+            Name = _name;   
+            Speed = _speed;
+        }
+        public static void RaceCars(Car car, List<Car> listofCars)
+        {
+            
+            Console.WriteLine("The {0} starts to race!", car.Name);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(car.Name);
+                Thread.Sleep(TimeSpan.FromSeconds(5));  // händelse ska inträffa vid särskilt intervall
+                CarTrouble(car);                        //metod för händelser
+                car.distancetoRace += 1;                
+            }
+            
+
+            listofCars.Add(car);  // när en bil är i mål läggs den till i listan
+            
+            Console.WriteLine("{0} has finished!", car.Name);
+        }
+        public static void CarTrouble(Car car)
+        {                       
+                Random random = new Random();
+
+                int n = random.Next(1, 51);
+                //int s = random.Next(1, 25);
+                //int p = random.Next(1, 11);
+                //int q = random.Next(1, 6);
+
+                if (n == 1)
+                {
+                    Console.WriteLine("Oops,{0} needs to refuel! 30 s delay!", car.Name);
+                    Thread.Sleep(3000);
+                }
+                if (n <= 2)
+                {
+                    Console.WriteLine("Squuech,{0} seems to be in need of new tires! 20 s delay", car.Name);
+                    Thread.Sleep(2000);
+                }
+                if (n <= 5)
+                {
+                    Console.WriteLine("Holy ***, {0} just smashed a bird on the windshield...10 s delay", car.Name);
+                    Thread.Sleep(1000);
+                }
+                if (n <= 10)
+                {
+                    car.Speed = car.Speed - 1;
+                    Console.WriteLine("Oh, {0} seems to be having some trouble with the engine, speed must be down to {1} km/h", car.Name, car.Speed);
+                    Thread.Sleep(10);
+                }        
+        }        
+    }  
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Car_Race_Threads;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        Car car1 = new Car("Toyota", 120);
+        Car car2 = new Car("Ford", 120);
+        Car car3 = new Car("Kia", 120);              
+
+        List<Car> listofCars = new List<Car>();
+
+        Console.WriteLine("The race is about to start!");
+        Console.WriteLine("For status of the race, press any key at any time");
+
+        Thread cc1 = new Thread(() => { Car.RaceCars(car1,listofCars); });  
+        Thread cc2 = new Thread(() => { Car.RaceCars(car2, listofCars); });
+        Thread cc3 = new Thread(() => { Car.RaceCars(car3, listofCars); });
+
+        Thread cc4 = new Thread(() => { CheckStatus(car1, car2, car3); });
+
+        cc1.Start();
+        cc2.Start();
+        cc3.Start();
+        cc4.Start();
+        
+
+        cc1.Join();
+        cc2.Join();
+        cc3.Join();
+
+        Console.WriteLine("The race is finished!!");
+
+        if (listofCars.Count > 0)       // den bil som ligger först i listan skrivs ut som vinnare
+        {
+            Console.WriteLine("The winner is: {0}! Congratulations!", listofCars[0].Name);
+        }
+      
+    }
+
+    public static void CheckStatus(Car car1, Car car2, Car car3)  
+    {
+        while (car1.distancetoRace !=10)
+        {
+
+            Console.ReadKey();  //tar vilken knapptryckning som helst
+
+            Console.WriteLine("{0}: Distance: {1} km Speed:{2}", car1.Name, car1.distancetoRace, car1.Speed);
+            Console.WriteLine("{0}: Distance: {1} km Speed:{2}", car2.Name, car2.distancetoRace, car2.Speed);
+            Console.WriteLine("{0}: Distance: {1} km Speed:{2}", car3.Name, car3.distancetoRace, car3.Speed);
+        }
+        
+    }
+}
